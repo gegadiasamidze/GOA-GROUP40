ffbee2b9769623c72d5f415ffb05e0e9ed24897e
gegadiasamidze <gegadiasamidze2011@gmail.com>
2025-03-26 20:58:33 +0400

    second commit

---
+++ day36/homework/hw.py
@@ -0,0 +1,7 @@
+def find_max(numbers):
+    return max(numbers)
+
+# ტესტირება
+numbers_list = [3, 10, 5, 8, 25, 15]
+result = find_max(numbers_list)
+print(result)  # ამ შემთხვევაში უნდა დაბეჭდოს: 25
---
+++ day36/homework/hw1.py
@@ -0,0 +1,7 @@
+def find_min(numbers):
+    return min(numbers)
+
+# ტესტირება
+numbers_list = [3, 10, 5, 8, 25, 15]
+result = find_min(numbers_list)
+print(result)  # ამ შემთხვევაში უნდა დაბეჭდოს: 3
